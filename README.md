b802886cbe17f96747363b92add42f62b7d77079
Marc Feliu <mfeliu@adncomunicacio.com>
2015-11-24 18:15:04 +0100

    Project initialized

---
+++ README.md
@@ -0,0 +1,30 @@
+# VR Image
+
+VR Experience with still images and Google Cardboard
+
+## Overview
+
+This project has been made with edge technologies like ECMASCRIPT6 - with [Babel](https://babeljs.io/) - or [Browserify](http://browserify.org/) to handle modules. All tied togheter with [Gulp](http://gulpjs.com/).
+To use it just follow installation instructions
+
+## Installation
+
+This project uses [NodeJS](http://nodejs.org), [Gulp](http://gulpjs.com/), [Ruby](http://rubyinstaller.org/) & [SASS](http://sass-lang.com/)
+
+Install node & ruby with the links above. After that install SASS with:
+
+```
+gem install sass
+```
+
+Finally install Gulp and other dependencies with:
+
+```
+npm install
+```
+
+When finished use `gulp` to start the development environment. If you just want compile for production use `gulp compile` 
+
+## TO DO
+
+* Make a browser minimum capabilities to allow the experience
---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+'use strict';
+var del        = require('del'),
+	gulp       = require('gulp'),
+	babel      = require('babelify'),
+	sass       = require('gulp-ruby-sass'),
+	browserify = require('gulp-browatchify'),
+	source     = require('vinyl-source-stream'),
+	uglify     = require('uglifyify'),
+	sync       = require('browser-sync');
+
+/* Paths */
+var paths = {
+	source: {
+		html: './src/index.html',
+		js: './src/js/main.js',
+		css: './src/scss/',
+		images: ['./src/images/**/*.png','./src/images/**/*.jpg','./src/images/**/*.gif']
+	},
+	dest : {
+		html: './bin',
+		js: './bin/js',
+		css: './bin/css',
+		images: './bin/images'
+	}
+}
+
+/* Cleaning tasks */
+gulp.task('clean-html',function(){
+	return del(paths.dest.html + '/*.html');
+});
+
+gulp.task('clean-images',function(){
+	return del([paths.dest.images + '/**/*.png',paths.dest.images + '/**/*.jpg',paths.dest.images + '/**/*.gif']);
+});
+
+
+/* Build tasks */
+gulp.task('html',['clean-html'],function(){
+	return gulp.src(paths.source.html)
+		   .pipe(gulp.dest(paths.dest.html))
+		   .on('end',function(){sync.reload()});
+});
+
+gulp.task('js',function(){
+	return gulp.src(paths.source.js)
+		   .pipe(browserify({experimental:true,debug:false,transforms:[babel,uglify]}))
+		   .on('error',function(err){console.log(err)})
+		   .pipe(source('bundle.js'))
+		   .pipe(gulp.dest(paths.dest.js))
+		   .on('end',function(){sync.reload()});
+});
+
+gulp.task('styles',function(){
+	return sass(paths.source.css + "/styles.scss",{
+		style:'compressed',
+		compass: false})
+		.pipe(gulp.dest(paths.dest.css))
+		.on('end',function(){sync.reload('styles.css')});
+});
+
+gulp.task('images',['clean-images'],function(){
+	return gulp.src(paths.source.images)
+		   .pipe(gulp.dest(paths.dest.images))
+		   .on('end',function(){sync.reload()});
+});
+
+/* Watch tasks */
+gulp.task('watch',function(){
+	gulp.watch('./src/js/**/*.js',['js']);
+	gulp.watch(paths.source.images,['images']);
+	gulp.watch(paths.source.html,['html']);
+	gulp.watch(paths.source.css + '/**/*.scss',['styles']);
+});
+
+
+gulp.task('server',function(){
+	sync({
+		server:{
+			baseDir:'./bin'
+		}
+	})
+});
+
+gulp.task('compile',['html','js','styles','images']);
+gulp.task('run',['compile','watch']);
+gulp.task('default',['run','server']);
